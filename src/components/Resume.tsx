73e0332f77a574547bb51e7b1aa5d8bdf2f32d28
Hannah Fitzsimmons <hannahfitzsimmons@gmail.com>
2024-08-21 23:01:30 -0400

    remove responsive pdf for now

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,7 +1,6 @@
 import { Paper } from '@mantine/core';
 import { pdfjs, Document, Page } from 'react-pdf';
 import React, { useState } from 'react';
-import '@/styles/pdf.scss';
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
 	'pdfjs-dist/build/pdf.worker.min.mjs',
@@ -13,7 +12,6 @@ interface ResumeProps {
 }
 
 const Resume: React.FC<ResumeProps> = ({ filepath }) => {
-	console.log('filepath', filepath);
 	const [numPages, setNumPages] = useState<number | null>(null);
 
 	const onDocumentLoadSuccess = ({ numPages }: { numPages: number }) => {
@@ -22,10 +20,10 @@ const Resume: React.FC<ResumeProps> = ({ filepath }) => {
 	};
 
 	return (
-		<Paper shadow="xs" p="md" className="pdf-paper">
-			<Document file={filepath} onLoadSuccess={onDocumentLoadSuccess} className="pdf-document">
-				{Array.from(new Array(numPages), (el, index) => (
-					<Page key={`page_${index + 1}`} pageNumber={index + 1} className="pdf-page" />
+		<Paper shadow="xs" p="md">
+			<Document file={filepath} onLoadSuccess={onDocumentLoadSuccess}>
+				{Array.from(new Array(numPages), (_el, index) => (
+					<Page key={`page_${index + 1}`} pageNumber={index + 1} />
 				))}
 			</Document>
 		</Paper>
