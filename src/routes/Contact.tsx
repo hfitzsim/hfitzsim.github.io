77ef7d0245c72c46df10336bd595add09527fab6
Hannah Fitzsimmons <hannahfitzsimmons@gmail.com>
2024-08-22 23:46:09 -0400

    set up email service

--- src/routes/Contact.tsx
+++ src/routes/Contact.tsx
@@ -1,9 +1,23 @@
-import { Stack, Group, Flex, Title, Button, TextInput, Textarea } from '@mantine/core';
+import {
+	Container,
+	Box,
+	Stack,
+	Group,
+	Flex,
+	Title,
+	Button,
+	TextInput,
+	Textarea,
+} from '@mantine/core';
 import { useForm } from '@mantine/form';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import emailjs from '@emailjs/browser';
+import { useState } from 'react';
 
 const Contact = () => {
+	const [isNotificationVisible, setIsNotifcationVisible] = useState<boolean>(false);
+
 	const form = useForm({
 		mode: 'uncontrolled',
 		initialValues: {
@@ -17,35 +31,69 @@ const Contact = () => {
 			message: (value) => (value.length < 5 ? 'Message must have at least 5 characters' : null),
 		},
 	});
+
+	const handleSubmit = (values: typeof form.values) => {
+		emailjs.init('a5m24FlFLLiGb_DHu');
+		emailjs
+			.send(
+				'service_rbwxoxj',
+				'template_64zleeo',
+				{
+					from_name: values.name,
+					to_name: 'Hannah',
+					message: values.message,
+					reply_to: values.email,
+				},
+				'a5m24FlFLLiGb_DHu'
+			)
+			.then((response) => {
+				console.log('SUCCESS!', response.status, response.text);
+				setIsNotifcationVisible(true);
+			})
+			.catch((err) => {
+				console.log('FAILED...', err);
+			});
+	};
 	return (
-		<Stack w={400} mt={50}>
-			<Group mb={50}>
-				<Button
-					variant="outline"
-					leftSection={<FontAwesomeIcon icon={faLinkedinIn} />}
-					onClick={() => (window.location.href = 'https://www.linkedin.com/in/hfitzsim/')}
-				>
-					LinkedIn
-				</Button>
-				<Button
-					leftSection={<FontAwesomeIcon icon={faGithub} />}
-					onClick={() => (window.location.href = 'https://github.com/hfitzsim')}
-				>
-					GitHub
-				</Button>
-			</Group>
-			<Title order={4}>Send me a message:</Title>
-			<form>
-				<Stack gap="xs">
-					<TextInput label="Name" {...form.getInputProps('name')} key={form.key('name')} />
-					<TextInput label="Email" {...form.getInputProps('email')} key={form.key('email')} />
-					<Textarea label="Message" {...form.getInputProps('message')} key={form.key('message')} />
-					<Flex justify="flex-end">
-						<Button type="submit">Submit</Button>
-					</Flex>
-				</Stack>
-			</form>
-		</Stack>
+		<Container>
+			<Stack w={400} mt={50}>
+				<Group mb={50}>
+					<Button
+						variant="outline"
+						leftSection={<FontAwesomeIcon icon={faLinkedinIn} />}
+						onClick={() => (window.location.href = 'https://www.linkedin.com/in/hfitzsim/')}
+					>
+						LinkedIn
+					</Button>
+					<Button
+						leftSection={<FontAwesomeIcon icon={faGithub} />}
+						onClick={() => (window.location.href = 'https://github.com/hfitzsim')}
+					>
+						GitHub
+					</Button>
+				</Group>
+				<Title order={4}>Send me a message:</Title>
+				<form onSubmit={form.onSubmit(handleSubmit)}>
+					<Stack gap="xs">
+						<TextInput label="Name" {...form.getInputProps('name')} key={form.key('name')} />
+						<TextInput label="Email" {...form.getInputProps('email')} key={form.key('email')} />
+						<Textarea
+							label="Message"
+							{...form.getInputProps('message')}
+							key={form.key('message')}
+						/>
+						<Flex justify="flex-end">
+							<Button type="submit">Submit</Button>
+						</Flex>
+					</Stack>
+				</form>
+			</Stack>
+			{isNotificationVisible && (
+				<Box mt={25} c="green" style={{ borderRadius: '5px', fontWeight: 700 }}>
+					Thank you for your message! I'll reply when I see it :)
+				</Box>
+			)}
+		</Container>
 	);
 };
 
